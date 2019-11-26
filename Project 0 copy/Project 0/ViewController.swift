6751a76a44bdb343215598b95747de3d4b86bf96
Will Landrieu <willlandrieu@gmail.com>
2019-11-26 11:26:32 -0500

    11/26/2019 changes
    Changes made in class.
    
    Fixed issue where clicking "home" or "switch org" would open safari.

--- Project 0 copy/Project 0/ViewController.swift
+++ Project 0 copy/Project 0/ViewController.swift
@@ -26,23 +26,4 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         view.addSubview(webView)
     }
-
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if navigationAction.navigationType == .linkActivated  {
-            if let url = navigationAction.request.url,
-                let host = url.host, !host.hasPrefix("www.google.com"),
-                UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url)
-                print(url)
-                print("Redirected to browser. No need to open it locally")
-                decisionHandler(.cancel)
-            } else {
-                print("Open it locally")
-                decisionHandler(.allow)
-            }
-        } else {
-            print("not a user click")
-            decisionHandler(.allow)
-        }
     }
-}
